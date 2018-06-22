e3a5245b87348978f6d99646b4c4e04ed17d83a1
moooofly <centos.sf@gmail.com>
2018-06-22 19:28:52 +0800

    update scripts/binPack.sh

--- scripts/binPack.sh
+++ scripts/binPack.sh
@@ -1,7 +1,18 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-echo "===> Packaging v$(cat VERSION)-bin.tar.gz ..."
+echo "----- build harbor-go-client for packaging -----"
+go build -v ../
+echo "------------------------------------------------"
 echo
-tar czvf v$(cat VERSION)-bin.tar.gz harbor-go-client config.yaml rp.yaml
+
+VER=$(cat ../VERSION)
+
+echo "===> Packaging ${VER}-bin.tar.gz"
+echo
+tar czvf ${VER}-bin.tar.gz harbor-go-client config.yaml rp.yaml
 echo
 echo "===> Packaging complete."
+echo
+
+echo "----- remove harbor-go-client -----"
+rm harbor-go-client
